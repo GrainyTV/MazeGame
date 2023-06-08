19c756e089e9db2de10b545b5f003f7b5c8bad0d
Nagy Árpád <108174632+GrainyTV@users.noreply.github.com>
2023-06-08 07:02:23 +0200

    Add files via upload

--- MazeGame/sources/Atalakitas.c
+++ MazeGame/sources/Atalakitas.c
@@ -4,62 +4,58 @@
 
 void Atalakitas(int x, int y, int meret, int *szomszedos_cellak, bool **palya)
 {
-    int n = 0;
+	int n = 0;
 
-    if(y != 1 && n == 0)
-    {
-        switch(*(szomszedos_cellak + (y-2)*meret + x))
-        {
-        case 3:
-            palya[(y-2+y)/2][x] = true;
-            //*(palya + ((y-2+y)/2)*meret + x) = true;
-            ++n;
-            break;
-        default:
-            break;
-        }
-    }
+	if(y != 1 && n == 0)
+	{
+		switch(*(szomszedos_cellak + (y-2)*meret + x))
+		{
+		case 3:
+			palya[(y-2+y)/2][x] = true;
+			++n;
+			break;
+		default:
+			break;
+		}
+	}
 
-    if(y != meret-2 && n == 0)
-    {
-        switch(*(szomszedos_cellak + (y+2)*meret + x))
-        {
-        case 3:
-            palya[(y+2+y)/2][x] = true;
-           // *(palya + ((y+2+y)/2)*meret + x) = true;
-            ++n;
-            break;
-        default:
-            break;
-        }
-    }
+	if(y != meret-2 && n == 0)
+	{
+		switch(*(szomszedos_cellak + (y+2)*meret + x))
+		{
+		case 3:
+			palya[(y+2+y)/2][x] = true;
+			++n;
+			break;
+		default:
+			break;
+		}
+	}
 
-    if(x != 1 && n == 0)
-    {
-       switch(*(szomszedos_cellak + y*meret + (x-2)))
-       {
-       case 3:
-           palya[y][(x-2+x)/2] = true;
-           // *(palya + y*meret + ((x-2+x)/2)) = true;
-            ++n;
-            break;
-       default:
-            break;
+	if(x != 1 && n == 0)
+	{
+		switch(*(szomszedos_cellak + y*meret + (x-2)))
+		{
+		case 3:
+			palya[y][(x-2+x)/2] = true;
+			++n;
+			break;
+		default:
+			break;
 
-       }
-    }
+		}
+	}
 
-    if(x != meret-2 && n == 0)
-    {
-        switch(*(szomszedos_cellak + y*meret + (x+2)))
-        {
-        case 3:
-            palya[y][(x+2+x)/2] = true;
-            //*(palya + y*meret + ((x+2+x)/2)) = true;
-            ++n;
-            break;
-        default:
-            break;
-        }
-    }
+	if(x != meret-2 && n == 0)
+	{
+		switch(*(szomszedos_cellak + y*meret + (x+2)))
+		{
+		case 3:
+			palya[y][(x+2+x)/2] = true;
+			++n;
+			break;
+		default:
+			break;
+		}
+	}
 }
